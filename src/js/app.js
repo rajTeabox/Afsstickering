a53c52f7830214f911214c149f4df79818c3604d
rajTeabox <bobbilirajashekarreddys@gmail.com>
2019-07-28 22:23:37 +0530

    Desktop completed except micellenious bugs

--- src/js/app.js
+++ src/js/app.js
@@ -47,6 +47,23 @@ $(document).ready(function () {
             }
         ]
     });
+
+    // FAQ
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+      });
+    }
+
 });
 
 
