703a309fff99f2540971d7566d42a9a38bded316
rajTeabox <bobbilirajashekarreddys@gmail.com>
2019-07-30 00:59:24 +0530

    some more progress

--- src/js/app.js
+++ src/js/app.js
@@ -4,14 +4,14 @@ $(document).ready(function () {
         dots:true
     }).on("afterChange", function (event){
         let index = $(".slick-dots .slick-active").index()+1;
-        $("#mini-refs img").removeClass("active")
-        $("#mini-refs .img:nth-child("+index+")").children(0).addClass('active')
+        $("#mini-refs img").removeClass("active");
+        $("#mini-refs .img:nth-child("+index+")").children(0).addClass('active');
     });
     $("#mini-refs .img").click(function (event) {
         const index = $("#mini-refs .img").index(this);
         $(".slick-dots li").eq(index).trigger('click');
-        $("#mini-refs img").removeClass("active")
-        $(this).children(0).addClass("active")
+        $("#mini-refs img").removeClass("active");
+        $(this).children(0).addClass("active");
     });
 
     $('#testimonialSlider').slick({
