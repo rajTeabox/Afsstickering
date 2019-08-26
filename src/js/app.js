1c0e41fa12d43497d4918e6bfd1a1d55083fa5c7
rajTeabox <bobbilirajashekarreddys@gmail.com>
2019-08-26 10:14:30 +0530

    services

--- src/js/app.js
+++ src/js/app.js
@@ -196,6 +196,15 @@ $(document).ready(function () {
         ]
       }
     );
+
+    $('.one-time').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true
+    });
+    
 });
 
 
